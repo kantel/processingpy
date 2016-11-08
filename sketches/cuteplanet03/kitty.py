c9101737fe5789edc40804a2eb35fdc1c48f58f5
Jörg Kantel <joerg@kantel.de>
2016-11-08 20:24:15 +0100

    Coding UTF-8

--- sketches/cuteplanet03/kitty.py
+++ sketches/cuteplanet03/kitty.py
@@ -1,16 +1,18 @@
+# coding=utf-8
+
 class Kitty(object):
-    def __init__(self, temp_x, temp_y):
-        self.x = temp_x
-        self.y = temp_y
-        self.radius_x = 50  # Bildbreite/2
-        self.radius_y = 85  # Bildhoehe/2
+    def __init__(self, tempX, tempY):
+        self.x = tempX
+        self.y = tempY
+        self.radiusX = 50  # Bildbreite/2
+        self.radiusY = 85  # Bildhöhe/2
         
     def loadPic(self):
         self.img = loadImage("horngirl.png")
     
     def move(self):
-        self.x = mouseX - self.radius_x
-        self.y = mouseY - self.radius_y
+        self.x = mouseX - self.radiusX
+        self.y = mouseY - self.radiusY
         
     def display(self):
         image(self.img, self.x, self.y)
