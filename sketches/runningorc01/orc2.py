4bfe88d981f48b3ad1653192ef9c74643b380b9a
Jörg Kantel <joerg@kantel.de>
2016-11-19 19:52:16 +0100

    Running Orc 1 hinzugefügt

--- sketches/runningorc01/orc2.py
+++ sketches/runningorc01/orc2.py
@@ -3,23 +3,62 @@ class Orc():
     def __init__(self, posX, posY):
         self.x = posX
         self.y = posY
+        self.dir = 1
+        self. dx = 0
         self.dy = 0
 
     def loadPics(self):
-        self.orc1 = loadImage("orc1.png")
-        self.orc2 = loadImage("orc2.png")
-        self.orc3 = loadImage("orc3.png")
+        self.orcrt1 = loadImage("orcrt1.gif")
+        self.orcrt2 = loadImage("orcrt2.gif")
+        self.orcfr1 = loadImage("orcfr1.gif")
+        self.orcfr2 = loadImage("orcfr2.gif")
+        self.orclf1 = loadImage("orclf1.gif")
+        self.orclf2 = loadImage("orclf2.gif")
+        self.orcbk1 = loadImage("orcbk1.gif")
+        self.orcbk2 = loadImage("orcbk2.gif")
     
     def move(self):
-        self.y += self.dy
-        if self.y >= height + 48:
-            self.y = -48
-            self.x = random(width-48)
+        if self.dir == 0:
+            if self.x >= width - 32:
+                self.x = width - 32
+                self.image1 = self.orcrt1
+                self.image2 = self.orcrt1
+            else:
+                self.x += self.dx
+                self.image1 = self.orcrt1
+                self.image2 = self.orcrt2
+        elif self.dir == 1:
+            if self.y >= height - 32:
+                self.y = height - 32
+                self.image1 = self.orcfr1
+                self.image2 = self.orcfr1
+            else:
+                self.y += self.dy
+                self.image1 = self.orcfr1
+                self.image2 = self.orcfr2
+        elif self.dir == 2:
+            if self.x <= 0:
+                self.x = 0
+                self.image1 = self.orclf1
+                self.image2 = self.orclf1
+            else:
+                self.x -= self.dx
+                self.image1 = self.orclf1
+                self.image2 = self.orclf2
+        elif self.dir == 3:
+            if self.y <= 0:
+                self.y = 0
+                self.image1 = self.orcbk1
+                self.image2 = self.orcbk1
+            else:
+                self.y -= self.dy
+                self.image1 = self.orcbk1
+                self.image2 = self.orcbk2
     
     def display(self):
-        if frameCount % 4 == 1:
-            image(self.orc1, self.x, self.y)
-        elif frameCount % 4 == 3:
-            image(self.orc3, self.x, self.y)
+        if frameCount % 8 >= 4:
+            image(self.image1, self.x, self.y)
         else:
-            image(self.orc2, self.x, self.y)
+            image(self.image2, self.x, self.y)
+    
+ 
--- sketches/runningorc01/runningorc01.pyde
+++ sketches/runningorc01/runningorc01.pyde
@@ -1,1 +1,31 @@
 # Running Orc 01
+
+from orc2 import Orc
+
+orc = Orc(160, -32)
+
+
+def setup():
+    global bg
+    bg = loadImage("field.png")
+    frameRate(30)
+    size(320, 320)
+    orc.loadPics()
+    orc.dx = 2
+    orc.dy = 2
+    
+def draw():
+    background(bg)
+    orc.move()
+    orc.display()
+    
+def keyPressed():
+    if keyPressed and key == CODED:
+        if keyCode == RIGHT:
+            orc.dir = 0
+        elif keyCode == DOWN:
+            orc.dir = 1
+        elif keyCode == LEFT:
+            orc.dir = 2
+        elif keyCode == UP:
+            orc.dir = 3
