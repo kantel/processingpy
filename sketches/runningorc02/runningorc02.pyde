11de3127ffccdc720af11ca7807c54a1ebd53eaa
Jörg Kantel <joerg@kantel.de>
2016-12-10 19:35:05 +0100

    Orc 2 fertig!

--- sketches/runningorc02/runningorc02.pyde
+++ sketches/runningorc02/runningorc02.pyde
@@ -1,14 +1,32 @@
 # Running Orc 02
-
-from orc3 import Orc
+from sprite2 import Orc, Wall, Lava
 tilesize = 32
 
-orc = Orc(8*tilesize, 0)
 
+obstacles = [[9,9,9,9,9,9,9,9,0,9],
+             [9,0,0,0,9,0,0,0,0,9],
+             [9,9,0,0,0,0,0,0,0,9],
+             [8,9,9,9,0,0,9,9,9,9],
+             [9,9,0,0,0,0,0,0,0,9],
+             [9,0,0,0,0,0,0,0,0,9],
+             [9,0,9,9,9,0,0,9,9,9],
+             [9,0,9,0,0,0,0,0,9,8],
+             [9,9,9,9,0,9,0,0,9,8],
+             [8,8,8,9,0,9,9,9,9,8]]
+
+orc = Orc(8*tilesize, 0)
+wall = []
+lava = []
 
 def setup():
     global bg
+    # bg = loadImage("ground0.png")
     bg = loadImage("ground.png")
+    loadObstaclesData()
+    # for i in range(len(wall)):
+    #     wall[i].loadPics()
+    # for i in range(len(lava)):
+    #     lava[i].loadPics()
     frameRate(30)
     size(320, 320)
     orc.loadPics()
@@ -17,7 +35,22 @@ def setup():
     
 def draw():
     background(bg)
+    # for i in range(len(wall)):
+    #     wall[i].display()
+    # for i in range(len(lava)):
+    #     lava[i].display()
     orc.move()
+    for i in range(len(wall)):
+        if orc.checkCollision(wall[i]):
+            if orc.dir == 0:
+                orc.x -= orc.dx
+            elif orc.dir == 1:
+                orc.y -= orc.dy
+            elif orc.dir == 2:
+                orc.x += orc.dx
+            elif orc.dir == 3:
+                orc.y += orc.dy
+            orc.image1 = orc.image2                            
     orc.display()
     
 def keyPressed():
@@ -30,3 +63,11 @@ def keyPressed():
             orc.dir = 2
         elif keyCode == UP:
             orc.dir = 3
+            
+def loadObstaclesData():
+    for y in range(10):
+        for x in range(10):
+            if obstacles[y][x] == 9:
+                wall.append(Wall(x*tilesize, y*tilesize))
+            elif obstacles[y][x] == 8:
+                lava.append(Lava(x*tilesize, y*tilesize))
--- sketches/runningorc02/sprite2.py
+++ sketches/runningorc02/sprite2.py
@@ -1,4 +1,8 @@
-class Orc(object):
+tw = 32
+th = 32
+tileSize = 32
+
+class Sprite(object):
 
     def __init__(self, posX, posY):
         self.x = posX
@@ -6,6 +10,15 @@ class Orc(object):
         self.dir = 1
         self.dx = 0
         self.dy = 0
+    
+    def checkCollision(self, otherSprite):
+        if (self.x < otherSprite.x + tw and otherSprite.x < self.x + tw
+            and self.y < otherSprite.y + th and otherSprite.y < self.y + th):
+            return True
+        else:
+            return False
+
+class Orc(Sprite):
 
     def loadPics(self):
         self.orcrt1 = loadImage("orcrt1.gif")
@@ -16,11 +29,11 @@ class Orc(object):
         self.orclf2 = loadImage("orclf2.gif")
         self.orcbk1 = loadImage("orcbk1.gif")
         self.orcbk2 = loadImage("orcbk2.gif")
-    
+        
     def move(self):
         if self.dir == 0:
-            if self.x >= width - 32:
-                self.x = width - 32
+            if self.x >= width - tileSize:
+                self.x = width - tileSize
                 self.image1 = self.orcrt2
                 self.image2 = self.orcrt2
             else:
@@ -28,8 +41,8 @@ class Orc(object):
                 self.image1 = self.orcrt1
                 self.image2 = self.orcrt2
         elif self.dir == 1:
-            if self.y >= height - 32:
-                self.y = height - 32
+            if self.y >= height - tileSize:
+                self.y = height - tileSize
                 self.image1 = self.orcfr2
                 self.image2 = self.orcfr2
             else:
@@ -54,11 +67,26 @@ class Orc(object):
                 self.y -= self.dy
                 self.image1 = self.orcbk1
                 self.image2 = self.orcbk2
-    
+                
     def display(self):
         if frameCount % 8 >= 4:
             image(self.image1, self.x, self.y)
         else:
             image(self.image2, self.x, self.y)
+            
+class Wall(Sprite):
     
- 
+    def loadPics(self):
+        self.pic = loadImage("wall.png")
+        
+    def display(self):
+        image(self.pic, self.x, self.y)
+
+class Lava(Sprite):
+    
+    def loadPics(self):
+        self.pic = loadImage("lava.png")
+        
+    def display(self):
+        image(self.pic, self.x, self.y)
+
