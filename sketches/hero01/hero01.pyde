efcac91ea0e98e2a760bb946bce3f3a17e1a4f07
Jörg Kantel <joerg@kantel.de>
2016-12-14 16:44:52 +0100

    Nun mit autonomen Ork

--- sketches/hero01/hero01.pyde
+++ sketches/hero01/hero01.pyde
@@ -1,5 +1,5 @@
 # Hero 01
-from sprites3 import Hero, Obstacle
+from sprites3 import Hero, Orc, Obstacle
 tilesize = 32
 
 terrain = [[0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,7],
@@ -13,7 +13,8 @@ terrain = [[0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,7],
            [9,6,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
            [6,6,6,6,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
 
-hero = Hero(8*tilesize, 0)
+hero = Hero(16*tilesize, 0)
+orc  = Orc(4*tilesize, 0)
 obstacles = []           
 
 def setup():
@@ -23,8 +24,11 @@ def setup():
     frameRate(30)
     size(640, 320)
     hero.loadPics()
+    orc.loadPics()
     hero.dx = 2
     hero.dy = 2
+    orc.dx = 2
+    orc.dy = 2
 
 def draw():
     background(bg)
@@ -39,8 +43,25 @@ def draw():
                 hero.x += hero.dx
             elif hero.dir == 3:
                 hero.y += hero.dy
-            hero.image1 = hero.image2                            
+            hero.image1 = hero.image2
     hero.display()
+    orc.move()
+    for i in range(len(obstacles)):
+        if orc.checkCollision(obstacles[i]):
+            if orc.dir == 0:
+                orc.x -= orc.dx
+                orc.dir = int(random(4))
+            elif hero.dir == 1:
+                orc.y -= orc.dy
+                orc.dir = int(random(4))
+            elif hero.dir == 2:
+                orc.x += orc.dx
+                orc.dir = int(random(4))
+            elif hero.dir == 3:
+                orc.y += orc.dy
+                orc.dir = int(random(4))
+            orc.image1 = orc.image2
+    orc.display()
 
 def keyPressed():
     if keyPressed and key == CODED:
--- sketches/hero01/sprites3.py
+++ sketches/hero01/sprites3.py
@@ -72,6 +72,60 @@ class Hero(Sprite):
             image(self.image1, self.x, self.y)
         else:
             image(self.image2, self.x, self.y)
+            
+class Orc(Sprite):
+
+    def loadPics(self):
+        self.orcrt1 = loadImage("orcrt1.gif")
+        self.orcrt2 = loadImage("orcrt2.gif")
+        self.orcfr1 = loadImage("orcfr1.gif")
+        self.orcfr2 = loadImage("orcfr2.gif")
+        self.orclf1 = loadImage("orclf1.gif")
+        self.orclf2 = loadImage("orclf2.gif")
+        self.orcbk1 = loadImage("orcbk1.gif")
+        self.orcbk2 = loadImage("orcbk2.gif")
+        
+    def move(self):
+        if self.dir == 0:
+            if self.x >= width - tileSize:
+                self.x = width - tileSize
+                self.dir = int(random(4))
+            else:
+                self.x += self.dx
+                self.image1 = self.orcrt1
+                self.image2 = self.orcrt2
+        elif self.dir == 1:
+            if self.y >= height - tileSize:
+                self.y = height - tileSize
+                self.y -= self.dy
+                self.dir = int(random(4))
+            else:
+                self.y += self.dy
+                self.image1 = self.orcfr1
+                self.image2 = self.orcfr2
+        elif self.dir == 2:
+            if self.x <= 0:
+                self.x = 0
+                self.dir = int(random(4))
+            else:
+                self.x -= self.dx
+                self.image1 = self.orclf1
+                self.image2 = self.orclf2
+        elif self.dir == 3:
+            if self.y <= 0:
+                self.y = 0
+                self.dir = int(random(4))
+            else:
+                self.y -= self.dy
+                self.image1 = self.orcbk1
+                self.image2 = self.orcbk2
+                
+    def display(self):
+        if frameCount % 8 >= 4:
+            image(self.image1, self.x, self.y)
+        else:
+            image(self.image2, self.x, self.y)
+
 
 class Obstacle(Sprite):
     
