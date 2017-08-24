994cf4082a706fa426ced18543c18c61f5523c6e
Jörg Kantel <joerg@kantel.de>
2017-08-24 16:17:34 +0200

    Avoider 3 (Sternenhimmel)

---
+++ sketches/avoider3/avoider3.pyde
@@ -0,0 +1,88 @@
+from random import randint
+from sprite import Skull, Smiley, Star
+
+w = 640
+h = 480
+tw = th = 36
+noSmileys = 10
+nobStars = 30
+startgame = True
+playgame = False
+gameover = False
+
+skull = Skull(w/2, 320)
+smiley = []
+for i in range(noSmileys):
+    smiley.append(Smiley(randint(0, w-tw), -randint(50, 250)))
+bStar = []
+for i in range(nobStars):
+    bStar.append(Star(randint(0, w-2), randint(2, h-2), 1, 0.1))
+
+def setup():
+    global heart
+    skull.score = 0
+    skull.health = 5
+    size(640, 480)
+    frameRate(30)
+    skull.loadPics()
+    for i in range(len(smiley)):
+        smiley[i].loadPics()
+        smiley[i].dy = randint(4, 10)
+    font = loadFont("ComicSansMS-32.vlw")
+    textFont(font, 32)
+    heart = loadImage("heart.png")
+    # noCursor()
+    # cursor(HAND)
+  
+def draw():
+    global heart
+    background(0, 0, 0)
+    text("Score: " + str(skull.score), 10, 32)
+    for i in range(skull.health):
+        image(heart, width - i*tw - tw - 2, 2)
+    if startgame:
+        startGame()
+    elif playgame:
+        playGame()
+    elif gameover:
+        gameOver()
+
+def startGame():
+    global startgame, playgame
+    text("Klick to Play", 200, height/2)
+    if mousePressed:
+        startgame = False
+        playgame = True
+        
+def playGame():
+    global playgame, gameover
+    for i in range(len(bStar)):
+        bStar[i].move()
+        bStar[i].display()
+    skull.move()
+    for i in range(len(smiley)):
+        if skull.checkCollision(smiley[i]):
+            if skull.health > 0:
+                skull.health -= 1
+                smiley[i].reset(randint(0, w-tw), -randint(50, 250))
+            else:
+                playgame = False
+                gameover = True 
+    skull.display()
+    for i in range(len(smiley)):
+        smiley[i].move()
+        if smiley[i].outside:
+            skull.score += 1
+        smiley[i].display()
+
+def gameOver():
+    global playgame, gameover
+    text("Game Over!", 200, height/2)
+    text("Klick to play again.", 200, 300)
+    if mousePressed:
+        gameover = False
+        for i in range(len(smiley)):
+            smiley[i].reset(randint(0, w-tw), -randint(50, 250))
+        playgame = True
+        skull.health = 5
+        
---
+++ sketches/avoider3/sprite.py
@@ -0,0 +1,89 @@
+from random import randint
+
+tw = th = 36
+
+class Sprite(object):
+    def __init__(self, posX, posY):
+        self.x = posX
+        self.y = posY
+
+    def checkCollision(self, otherSprite):
+        if (self.x < otherSprite.x + tw and otherSprite.x < self.x + tw
+            and self.y < otherSprite.y + th and otherSprite.y < self.y + th):
+            return True
+        else:
+            return False
+
+
+class Skull(Sprite):
+
+    def __init__(self, posX, posY):
+        super(Skull, self).__init__(posX, posY)
+        self.score = 0
+        self.health = 0
+            
+    def loadPics(self):
+        self.im1 = loadImage("skull.png")
+        
+    def move(self):
+        self.x = mouseX
+        if self.x <= 0:
+            self.x = 0
+        elif self.x >= width-tw:
+            self.x = width - tw
+            
+    def display(self):
+        image(self.im1, self.x, self.y)
+
+class Smiley(Sprite):
+    
+    def __init__(self, posX, posY):
+        super(Smiley, self).__init__(posX, posY)
+        self.outside = False
+
+    def loadPics(self):
+        self.im0 = loadImage("smiley0.png")
+        self.im1 = loadImage("smiley1.png")
+        self.im2 = loadImage("smiley4.png")
+        
+    def move(self):
+        self.outside = False
+        self.y += self.dy
+        if self.y >= height:
+            self.outside = True
+            self.y = -randint(50, 250)
+            self.x = randint(0, width-tw)
+            self.dy = randint(4, 10)
+        
+    def display(self):
+        if (self.y > -30) and (self.y <= 250):
+            image(self.im0, self.x, self.y)
+        elif (self.y > 250) and (self.y <= 320):
+            image(self.im1, self.x, self.y)
+        elif (self.y > 320):
+            image(self.im2, self.x, self.y)
+    
+    def reset(self, posX, posY):
+        self.x = posX
+        self.y = posY
+            
+class Star(object):
+    
+    def __init__(self, posX, posY, dia, speed):
+        self.x = posX
+        self.y = posY
+        self.r = dia
+        self.dy = speed
+    
+    def move(self):
+        self.outside = False
+        self.y += self.dy
+        if self.y >= height:
+            self.outside = True
+            self.y = -2*self.r
+            self.x = randint(0, width - 2*self.r)
+    
+    def display(self):
+        fill(255, 255, 255)
+        noStroke()
+        ellipse(self.x, self.y, self.r, self.r)
