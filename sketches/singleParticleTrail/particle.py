18cb252c793a9f8cc47bd74931313b168c46461e
Jörg Kantel <joerg@kantel.de>
2018-02-09 20:54:17 +0100

    Single Particle

---
+++ sketches/singleParticleTrail/particle.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+
+class Particle(object):
+    
+    def __init__(self, l):
+        self.acceleration = PVector(0, 0.05)
+        self.velocity = PVector(random(-1, 1), random(-2, 0))
+        self.location = l.get()
+        self.lifespan = 255.0
+    
+    def run(self):
+        self.update()
+        self.display()
+    
+    def update(self):
+        self.velocity.add(self.acceleration)
+        self.location.add(self.velocity)
+        self.lifespan -= 1.5
+    
+    def display(self):
+        stroke(0, self.lifespan)
+        fill(0, self.lifespan)
+        ellipse(self.location.x, self.location.y, 8, 8)
+        
+    def isDead(self):
+        if self.lifespan <= 0:
+            return True
+        else:
+            return False
