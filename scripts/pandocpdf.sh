ea9760d4f37b826d2329d957afff3f0235088785
Jörg Kantel <joerg@kantel.de>
2018-06-01 20:25:26 +0200

    Layout-Experimente

--- scripts/pandocpdf.sh
+++ scripts/pandocpdf.sh
@@ -2,4 +2,4 @@
 PATH=$PATH:/usr/texbin:/usr/local/bin
 
 cd "$TM_PROJECT_DIRECTORY"/book
-pandoc --latex-engine=xelatex book.md -o book.pdf --chapters --highlight-style tango --toc -V geometry=a4paper,left=3cm,right=2cm,bottom=2cm,top=2cm  -V lang=german -H myheader.tex -V fontsize=12pt
+pandoc --latex-engine=xelatex book.md -o book.pdf --chapters --highlight-style tango --toc -V geometry=a4paper,left=3cm,right=3cm,bottom=3cm,top=3cm  -V lang=german -H myheader.tex -V fontsize=12pt
