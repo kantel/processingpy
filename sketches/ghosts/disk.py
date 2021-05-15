192121c9fc989e430d1ea65ed22d6fe6fdcfe9a6
Jörg Kantel <joerg@kantel.de>
2021-05-15 20:58:20 +0200

    Bubble Busters

--- sketches/ghosts/disk.py
+++ sketches/ghosts/disk.py
@@ -1,20 +1,39 @@
+from random import randint
+
 class Disk():
     
-    def __init__(self, ax_p, ay_p, ax_d, ay_d, a_r, a_clr):
-        self.pos = PVector(ax_p, ay_p)
-        self.dir = PVector(ax_d, ay_d)
-        self.radius = a_r
-        self.clr = a_clr
+    def __init__(self, _x, _y, _ax, _ay, _r, _clr):
+        self.pos = PVector(_x, _y)
+        self.dir = PVector(_ax, _ay)
+        self.radius = _r
+        self.clr = _clr
     
     def update(self):
-        self.pos += self.dir
-        if (self.pos.x - self.radius <= 0) or (self.pos.x + self.radius >= width):
+        self.pos.add(self.dir)
+        if (self.pos.x - 2*self.radius <= 0):
+            self.pos.x = 2*self.radius
             self.dir.x *= -1
-            self.pos.x += 2*self.dir.x
-        if (self.pos.y - self.radius <= 0) or (self.pos.y + self.radius >= height):
+        if (self.pos.x + 2*self.radius >= width):
+             self.pos.x = width - 2*self.radius
+             self.dir.x *= -1
+        if (self.pos.y - 2*self.radius <= 0):
+            self.pos.y = 2*self.radius
             self.dir.y *= -1
-            self.pos.y += 2*self.dir.y
+        if (self.pos.y + 2*self.radius >= height):
+             self.pos.y = height - 2*self.radius
+             self.dir.y *= -1
     
     def show(self):
         fill(self.clr)
-        circle(self.pos.x, self.pos.y, self.radius)
+        circle(self.pos.x, self.pos.y, self.radius*2)
+    
+    def circle_collision(self, other):
+        if other != self:
+            distance = abs(dist(self.pos.x, self.pos.y, other.pos.x, other.pos.y))
+            if distance <= self.radius + other.radius:
+                self.dir *= -1
+                other.pos = PVector(randint(2*other.radius, width - 2*other.radius),
+                                    randint(2*other.radius, height - 2*other.radius))
+                other.dir *= -1
+                other.radius = randint(5, 15)
+                other.clr = color(randint(20, 250), randint(20, 250), randint(20, 250), 180)
--- sketches/ghosts/ghosts.pyde
+++ sketches/ghosts/ghosts.pyde
@@ -4,18 +4,24 @@ from disk import Disk
 WIDTH = 640
 HEIGHT = 480
 BORDER = 50
-N_DISKS = 10
+N_DISKS = 30
+SPEED = 4
+
+FPS = 60
 
 def setup():
     size(WIDTH, HEIGHT)
-    this.surface.setTitle("Ghost Busters")
+    this.surface.setTitle("Bubble Busters")
     ghost_factory()
+    frameRate(FPS)
     
 def draw():
     global disks
     background(132, 144, 163)
     for disk in disks:
         disk.update()
+        for other_disk in disks:
+            disk.circle_collision(other_disk)
         disk.show()
 
 def ghost_factory():
@@ -24,10 +30,14 @@ def ghost_factory():
     for _ in range(N_DISKS):
         x_pos = randint(BORDER, width - BORDER)
         y_pos = randint(BORDER, height - BORDER)
-        x_dir = randint(-8, 8)
-        y_dir = randint(-8, 8)
-        radius = randint(10, 30)
+        x_dir = randint(-SPEED, SPEED)
+        if x_dir == 0:
+            x_dir = 1
+        y_dir = randint(-SPEED, SPEED)
+        if y_dir == 0:
+            y_dir = -1
+        radius = randint(5, 15)
         clr = color(randint(20, 250), randint(20, 250), randint(20, 250), 180)
         disks.append(Disk(x_pos, y_pos, x_dir, y_dir, radius, clr))
-        
-    
+  
+      
